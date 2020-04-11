fbef992b669a75955891cfd92c85aa080b09f000
DavidCarricondo <dacsmail00@gmail.com>
2020-04-11 19:02:53 +0200

    Fixed dictionary funciton

--- src/ApiNapster_functions.py
+++ src/ApiNapster_functions.py
@@ -32,19 +32,34 @@ def getName(url, type):
 #Funcion that uses the other functions to create an artist dictionary with info
 def artist_dict(artist):
     json1 = getFromNapster(artist)
-    if len(json1['artists'])==0:
-        return None
-    #Try the attributes that are not allways in an artist json
+    #Some artists with non valid characters may be skipped
+    try:
+        if len(json1['artists'])==0:
+            return None
+    except: return None
+    #Try the attributes in case they are not in the artist json
     try:
         bio = BeautifulSoup(json1['artists'][0]['bios'][0]['bio'], 'html.parser').text
     except:
         bio = None
     try:
+        blurb = json1['artists'][0]['blurbs']
+    except:
+        blurb = None
+    try:
         similars = getName(json1['artists'][0]['links']['contemporaries']['href'],'artists')
     except:
         similars = None
+    try:
+        genres = getName(json1['artists'][0]['links']['genres']['href'],'genres')
+    except:
+        bio = None
+    try:
+        images: json1['artists'][0]['links']['images']['href']
+    except:
+        images = None
     return {'bio' : bio,
-            'blurb' : json1['artists'][0]['blurbs'],
+            'blurb' : blurb,
             'similars' : similars,
-            'genres' : getName(json1['artists'][0]['links']['genres']['href'],'genres'),
-            'images' : json1['artists'][0]['links']['images']['href']}
+            'genres' : genres,
+            'images' : images}
